7b53e1bde001d531d4c3c7bccf1d65d7d290a791
hruta2910 <72400088+hruta2910@users.noreply.github.com>
2021-09-23 19:43:05 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,39 @@
+class Launcher{
+	constructor(bodyA,pointB)
+	{
+		
+		var options={ bodyA:bodyA,			 
+			pointB:pointB, 
+			stiffness:0.004, 
+			length:10
+			
+		}
+		
+		//this.bodyA=body
+		this.pointB=pointB
+		this.launcher=Constraint.create(options)
+		World.add(world,this.launcher)
+	}
+
+	attach(body){
+		this.launcher.bodyA=body;
+	}
+
+	fly()
+	{
+		this.launcher.bodyA=null;
+	}
+
+	display()
+	{
+		if(this.launcher.bodyA)
+		{
+			var pointA=this.launcher.bodyA.position;
+			var pointB=this.pointB
+			push();
+			stroke("black");
+			strokeWeight(2);		
+			line(pointB.x,pointB.y,pointA.x,pointA.y);
+		}
+	}
+}
